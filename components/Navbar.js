7338248480246f72a25f13b9d7cd6fb0e34c2022
Neeraj <nxxwxj@gmail.com>
2022-02-24 12:09:20 +0530

    created the Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,35 @@
+import requests from '../utils/requests'
+import { useRouter } from 'next/router'
+
 const Navbar = () => {
+  const router = useRouter()
+
   return (
-    <div>Navbar</div>
+    <nav className="relative flex items-center">
+      <div
+        className="flex px-10 py-5 sm:px-20 text-xl whitespace-nowrap
+      space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide  "
+      >
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2
+            key={key}
+            onClick={() => router.push(`/?genre=${key}`)}
+            className="last:pr-12 cursor-pointer transition-100 transform 
+            hover:scale-125 hover:text-white active:text-red-500 "
+          >
+            {title}
+          </h2>
+        ))}
+      </div>
+      <div
+        className="absolute  left-0 bg-gradient-to-r from-[#06202a] 
+        h-10 w-1/12 "
+      />
+      <div
+        className="absolute  right-0 bg-gradient-to-l from-[#06202a]
+        h-10 w-1/12 "
+      />
+    </nav>
   )
 }
 
