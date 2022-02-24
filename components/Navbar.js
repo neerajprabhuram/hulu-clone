30bbf1d8034ded6c506d952f762119306931fc16
Neeraj <nxxwxj@gmail.com>
2022-02-24 22:44:12 +0530

    fixed Navbar fontsize and hiding issues

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,15 +7,16 @@ const Navbar = () => {
   return (
     <nav className="relative flex items-center">
       <div
-        className="flex px-10 py-5 sm:px-20 text-xl whitespace-nowrap
-      space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide  "
+        className="flex px-10 py-5 sm:px-20 text-base sm:text-lg md:text-xl
+        whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll 
+        scrollbar-hide"
       >
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
             onClick={() => router.push(`/?genre=${key}`)}
-            className="last:pr-12 cursor-pointer transition-100 transform 
-            hover:scale-125 hover:text-white active:text-red-500 "
+            className="last:pr-12 cursor-pointer hover:scale-110
+            hover:text-white active:text-red-500"
           >
             {title}
           </h2>
@@ -23,11 +24,11 @@ const Navbar = () => {
       </div>
       <div
         className="absolute  left-0 bg-gradient-to-r from-[#06202a] 
-        h-10 w-1/12 "
+        h-10 w-[5%]"
       />
       <div
         className="absolute  right-0 bg-gradient-to-l from-[#06202a]
-        h-10 w-1/12 "
+        h-10 w-1/12"
       />
     </nav>
   )
