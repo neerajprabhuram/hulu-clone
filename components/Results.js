ee001518290fafbb1fef6638d57989ef15486070
Neeraj <nxxwxj@gmail.com>
2022-02-24 20:53:18 +0530

    completed the Results component and added an animation

--- components/Results.js
+++ components/Results.js
@@ -1,12 +1,16 @@
-import Thumbnail from "./Thumbnail"
+import Thumbnail from './Thumbnail'
+import FlipMove from 'react-flip-move'
 
-const Results = ({results}) => {
+const Results = ({ results }) => {
   return (
-    <div>
+    <FlipMove
+      className="px-4 mt-10 sm:grid sm:grid-cols-2 lg:grid-cols-3 
+      xl:grid-cols-4 3xl:flex flex-wrap justify-center "
+    >
       {results.map(result => (
         <Thumbnail key={result.id} result={result} />
       ))}
-    </div>
+    </FlipMove>
   )
 }
 
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import { ThumbUpIcon } from '@heroicons/react/outline'
+import { forwardRef } from 'react'
 
-const Thumbnail = ({ result }) => {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original'
 
   return (
     <div
-      className="group cursor-pointer p-5 transition duration-200 ease-in
-    transform sm:hover:scale-105 hover:z-50"
+      ref={ref}
+      className="group cursor-pointer p-2 transition duration-200 ease-in
+      transform sm:hover:scale-105 hover:z-50"
     >
       <Image
         layout="responsive"
@@ -20,16 +22,16 @@ const Thumbnail = ({ result }) => {
       />
       <div className="p-2">
         <h2
-          className="mt-1 text-xl text-white transition-all duration-200
+          className="mt-1 text-xl text-white transition-all duration-100
           ease-in-out font-bold sm:font-normal sm:group-hover:font-bold"
         >
           {result.title || result.original_name}
         </h2>
         <p className="truncate max-w-md">{result.overview}</p>
         <p
-          className="flex items-center mt-2 text-sm sm:opacity-0 
-          group-hover:opacity-100 transition-all duration-200 ease-in-out 
-          capitalize"
+          className="flex items-center mt-1 text-gray-400 text-sm 
+          sm:opacity-0 group-hover:opacity-100 transition-all 
+          duration-200 ease-in-out capitalize"
         >
           {result.media_type && `${result.media_type} ~`}{' '}
           {result.release_date || result.first_air_date} ~{' '}
@@ -39,6 +41,6 @@ const Thumbnail = ({ result }) => {
       </div>
     </div>
   )
-}
+})
 
 export default Thumbnail
