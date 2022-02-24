b1e6a4b089190ba824c6075f0fed97e2fc1663e4
Neeraj <nxxwxj@gmail.com>
2022-02-24 18:49:01 +0530

    created the Thumbnail component

---
+++ components/Thumbnail.js
@@ -0,0 +1,44 @@
+import Image from 'next/image'
+import { ThumbUpIcon } from '@heroicons/react/outline'
+
+const Thumbnail = ({ result }) => {
+  const BASE_URL = 'https://image.tmdb.org/t/p/original'
+
+  return (
+    <div
+      className="group cursor-pointer p-5 transition duration-200 ease-in
+    transform sm:hover:scale-105 hover:z-50"
+    >
+      <Image
+        layout="responsive"
+        height={1080}
+        width={1920}
+        src={
+          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+          `${BASE_URL}${result.poster_path}`
+        }
+      />
+      <div className="p-2">
+        <h2
+          className="mt-1 text-xl text-white transition-all duration-200
+          ease-in-out font-bold sm:font-normal sm:group-hover:font-bold"
+        >
+          {result.title || result.original_name}
+        </h2>
+        <p className="truncate max-w-md">{result.overview}</p>
+        <p
+          className="flex items-center mt-2 text-sm sm:opacity-0 
+          group-hover:opacity-100 transition-all duration-200 ease-in-out 
+          capitalize"
+        >
+          {result.media_type && `${result.media_type} ~`}{' '}
+          {result.release_date || result.first_air_date} ~{' '}
+          <ThumbUpIcon className="h-4 mx-2" />
+          {result.vote_count}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Thumbnail
--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["links.papareact.com"]
-  }
+    domains: ['links.papareact.com', 'image.tmdb.org'],
+  },
 }
 
 module.exports = nextConfig
